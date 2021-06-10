8087160e8357f9d9235f0ce43f72a8dd4af21d94
DARSHAN P B <darshanpb111@gmail.com>
2021-06-10 12:01:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// Routes
+app.get('/shots/', function(req, res) {
+  res.send('Hello World!');
+});
+
+// Listen
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on localhost:'+ port);
