d07191444dde31ca70c1cddd43acb1bd457e2b51
Srikanth Katti <srikanthkatti.katti@gmail.com>
2021-06-10 12:44:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
+const shots = {
+"shots":[
+{
+"trailerContentId":"234ab~234cd~vod",
+"like":90,
+"dislike":0
+},
+{
+"trailerContentId":"123ab~123cd~vod",
+"like":57,
+"dislike":10
+},
+{
+"trailerContentId":"234ab~234cd~vod",
+"like":54,
+"dislike":9
+},
+{
+"trailerContentId":"234ab~234cd~vod",
+"like":6,
+"dislike":4
+},
+{
+"trailerContentId":"234ab~234cd~vod",
+"like":5,
+"dislike":25
+}
+]
+};
 
 // Routes
 app.get('/shots/', function(req, res) {
-  res.send('Hello World!');
+  res.send(shots);
 });
 
 // Listen
